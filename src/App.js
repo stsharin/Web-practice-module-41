0782190f357e996d6e762b116b086e1a3c0a249d
stsharin <stssharin@gmail.com>
2021-04-08 03:10:27 +0600

    onBlur added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,94 @@ import './App.css';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import { useState } from 'react';
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+else {
+  firebase.app();
+}
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  }) // object with multiple values
+
   const provider = new firebase.auth.GoogleAuthProvider();
   const handleSignIn = () => {
-    console.log('sign in clicked');
+    firebase.auth().signInWithPopup(provider)
+      .then(res => {
+        const { displayName, photoURL, email } = res.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(signedInUser);
+        // console.log(res);
+        // console.log(displayName, photoURL, email);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+      })
+  }
+  const handleSignOut = () => {
+    // console.log('singed out');
+    firebase.auth().signOut()
+      .then(res => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: '',
+          email: '',
+          photo: ''
+        }
+        setUser(signedOutUser);
+      })
+      .catch(err => {
+
+      })
+  }
+
+  const handleBlur = (e) => {
+    // detecting the field and value
+    console.log(e.target.name, e.target.value);
+  }
+
+  const handleSubmit = () => {
+
   }
 
   return (
     <div className="App">
-      <button onClick={handleSignIn}>Sign In</button>
+      {
+        // ? True Condition : false condition
+        user.isSignedIn ? <button onClick={handleSignOut}>Sign Out</button> :
+          <button onClick={handleSignIn}>Sign In</button>
+      }
+      {
+        user.isSignedIn && <div>
+          <p>Welcome, {user.name}</p>
+          <p>Your email: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
+      <h1>Our Own Authentication</h1>
+      <form onSubmit={handleSubmit}>
+        <input name="email" onBlur={handleBlur} type="text" id="" placeholder="Your Email" required/>
+        <br/>
+        <input name="password" onBlur={handleBlur} type="password" id="" placeholder="Your Password" required/>
+        <br/>
+        {/* <button>Submit</button> */}
+        <input type="submit" value="Submit"/>
+      </form>
     </div>
   );
 }
